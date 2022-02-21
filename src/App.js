99e7b6f498e69c04d24787408efc6cbc72926d05
Mohammed Falil <mohammed_falil@outlook.com>
2022-02-21 11:29:52 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Footer,Blog , Possibility,Features,WhatGPT3,Header } from "./containers";
+import {CTA,Brand ,Navbar} from "./Components";
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='gradient_bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/whatGPT3/WhatGPT.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WhatGPT() {
+  return (
+    <div>WhatGPT</div>
+  )
+}
+
+export default WhatGPT
