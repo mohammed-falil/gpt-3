cf046c4b2449dfdfab53aed8f8fee9b7821bde62
Mohammed Falil <mohammed_falil@outlook.com>
2022-04-01 15:05:03 +0530

    end

--- src/Components/cta/CTA.jsx
+++ src/Components/cta/CTA.jsx
@@ -5,7 +5,7 @@ function CTA() {
   return (
     <div className="gpt3_cta">
       <div className="gpt3_cta_content">
-        <p>Request Early Access toGet Started</p>
+        <p>Request Early Access to Get Started</p>
         <h3>Register today & start exploring the endless possibilities.</h3>
       </div>
       <div className="gpt3_cta_btn">
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import "./footer.css";
+import gpt3Logo from "../../assets/logo.svg";
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="gpt3_footer section_padding">
+      <div className="gpt3_footer_heading">
+        <h1 className="gradient_text">
+          Do you want to step to the future before others
+        </h1>
+      </div>
+      <div className="gpt3_footer_btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3_footer_links">
+        <div className="gpt3_footer_links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+            repellat.
+          </p>
+        </div>
+        <div className="gpt3_footer_links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3_footer_links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+
+        <div className="gpt3_footer_links_div">
+          <h4>Get in touch</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>024-45689078</p>
+          <p>info@mailme.com</p>
+        </div>
+      </div>
+      <div className="gpt3_footer_copyright">
+        <p>© 2022 GPT-3. All rights reserved</p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
